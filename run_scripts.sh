aea070d82e47eefafe4a86bc6f9ea7fc591769ff
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-02-02 17:10:51 +0900

    Remove unnecessary file transfer and update file handling in send_files.sh

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-source ./configs/config.sh
-DIRECTORY="./para"
+
+rm para/*.npy
 
 python3 src/inner_para.py
-
-find "$DIRECTORY" -name "*.npy" -exec sh -c '
-    file={};
-    echo "Sending '$file' to remote server";
-    sshpass -p "$REMOTE_PASS" scp "$file" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_MATRIX_DIR/$(basename "$file")"
-' \;
-
 python3 src/getRGBD.py
--- send_files.sh
+++ send_files.sh
@@ -2,13 +2,23 @@
 
 source ./configs/config.sh
 
-DIRECTORIES=("image")
+DIRECTORIES=("image" "matrix")
 
 for DIR in "${DIRECTORIES[@]}"
 do
-    inotifywait -m "$DIR" -e create -e moved_to |
-        while read path action file; do
-            echo "The file '$file' appeared in directory '$path' via '$action'"
-            sshpass -p "$REMOTE_PASS" scp "$path$file" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_BASE_DIR$DIR/"
-        done
+    if [ "$DIR" == "matrix" ]; then
+        inotifywait -m "$DIR" -e create -e moved_to --format '%w%f' |
+            while read file; do
+                if [[ $file == *.npy ]]; then
+                    echo "The file '$file' appeared in directory '$DIR' via 'create/moved_to'"
+                    sshpass -p "$REMOTE_PASS" scp "$file" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_BASE_DIR$DIR/"
+                fi
+            done
+    else
+        inotifywait -m "$DIR" -e create -e moved_to --format '%w%f' |
+            while read file; do
+                echo "The file '$file' appeared in directory '$DIR' via 'create/moved_to'"
+                sshpass -p "$REMOTE_PASS" scp "$file" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_BASE_DIR$DIR/"
+            done
+    fi
 done
