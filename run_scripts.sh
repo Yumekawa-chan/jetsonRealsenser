072c15cf4ddf6081b2648a305598683404a68938
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-02-02 16:58:09 +0900

    Fix file path in scp command

--- run_scripts.sh
+++ run_scripts.sh
@@ -7,7 +7,7 @@ python3 src/inner_para.py
 find "$DIRECTORY" -name "*.npy" -exec sh -c '
     file={};
     echo "Sending '$file' to remote server";
-    sshpass -p "$REMOTE_PASS" scp "$file" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_MATRIX_DIR$(basename "$file")"
+    sshpass -p "$REMOTE_PASS" scp "$file" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_MATRIX_DIR/$(basename "$file")"
 ' \;
 
 python3 src/getRGBD.py
