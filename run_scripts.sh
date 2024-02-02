aab77290703d1e9891d33eab7725e96a2a450475
Yumekawa-chan <20aj079@ms.dendai.ac.jp>
2024-02-02 16:21:47 +0900

    Add script to send files to remote server

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 python3 src/inner_para.py
 python3 src/getRGBD.py
+
+source ./configs/config.sh
+
+DIRECTORY="./para"
+
+find "$DIRECTORY" -name "*.npy" -exec sh -c '
+    file={};
+    echo "Sending '$file' to remote server";
+    sshpass -p "$REMOTE_PASS" scp "$file" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_BASE_DIR/matrix/$(basename "$file")"
+' \;
+
